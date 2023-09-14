d443e6d04dd2b82ccc0d24ba80d5d7f8aebbe7b4
duck_alan <duckalan2@gmail.com>
2023-09-14 23:51:04 +0300

    Add project files.

---
+++ CppStrings/CppStrings.cpp
@@ -0,0 +1,74 @@
+﻿#include "CppStrings.h"
+
+int main()
+{
+	string s1 = "456 - 433 = ?; 12 - 3 = ?; 8767 - 686 = ?; -453 - 3 = ?;";
+	string s2 = "456,433,12,3,8767,686,-453,3";
+	cout << ParseMathString(s1, s2) << '\n';
+
+	string s3 = "a1,bb2,ccc3,dddd4,eeeee5,ffffff6,ggggggg7,hhhhhhhh8,iiiiiiiii9,jjjjjjjjjj10";
+
+	auto substrings = SplitString(s3, ',');
+	for (auto& substring : substrings)
+	{
+		cout << substring << '\n';
+	}
+
+	system("pause");
+}
+
+string ParseMathString(const string& s1, const string& s2)
+{
+	auto strNumbers = SplitString(s2, ',');
+	string result = s1;
+	size_t searchOffset = 0;
+
+	for (size_t i = 0; i < strNumbers.size(); i += 2)
+	{
+		int firstOperand = stoi(strNumbers[i]);
+		int secondOperand = stoi(strNumbers[i + 1]);
+
+		size_t questionMarkIndex = result.find('?', searchOffset);
+
+		result.replace(questionMarkIndex, 1, to_string(firstOperand - secondOperand));
+
+		searchOffset = questionMarkIndex + 1;
+	}
+
+	return result;
+}
+
+vector<string> SplitString(const string& str, char separator)
+{
+	vector<string> result;
+
+	for (size_t i = 0; i < str.length(); )
+	{
+		size_t substrLength = GetSubstringLength(str, i, separator);
+
+		result.push_back(str.substr(i, substrLength));
+
+		i += substrLength + 1;
+	}
+
+	return result;
+}
+
+size_t GetSubstringLength(const string& str, size_t startIndex, char lastChar)
+{
+	size_t substrLength = 0;
+	size_t j = startIndex;
+
+	while (str[j] != lastChar && str[j] != str.length())
+	{
+		if (j == str.length())
+		{
+			break;
+		}
+
+		substrLength++;
+		j++;
+	}
+
+	return substrLength;
+}
---
+++ CppStrings/CppStrings.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::to_string;
+
+string ParseMathString(const string& s1, const string& s2);
+
+vector<string> SplitString(const string& str, char delimiter);
+
+/// <summary>
+/// Возвращает длину подстроки в строке <paramref name="str"/>, 
+/// начиная с индекса <paramref name="startIndex"/> и до первого попавшегося
+/// символа <paramref name="lastChar"/>.
+/// </summary>
+size_t GetSubstringLength(const string& str, size_t startIndex, char lastChar);
